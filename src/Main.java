faf48a95dac7c605304b3bd4997041bbd3e3b8ab
Alexia <trup.lexa@gmail.com>
2024-09-15 10:43:54 +0500

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2+3");
+        System.out.println(2+3);
+
+        int speed = 17;
+        int x = speed + 3;
+
+        double d = 6.5;
+
+        String name = "Plotva";
+        System.out.println(name + " везла со скоростью " + speed);
+
+        System.out.println(speed);
+    }
+}
